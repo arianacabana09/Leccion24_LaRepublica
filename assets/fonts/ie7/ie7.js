38a9dce2c1b3af58fa907d503760c8c4c1cc5c31
Ariana Cabana <acabana.0904@gmail.com>
2017-04-21 12:32:25 -0500

    subiendo archivos

---
+++ assets/fonts/ie7/ie7.js
@@ -0,0 +1,39 @@
+/* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
+/* The script tag referencing this file must be placed before the ending body tag. */
+
+/* Use conditional comments in order to target IE 7 and older:
+	<!--[if lt IE 8]><!-->
+	<script src="ie7/ie7.js"></script>
+	<!--<![endif]-->
+*/
+
+(function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+		'icon-chevron-down': '&#xf0a3;',
+		'icon-play3': '&#xea1c;',
+		'icon-share2': '&#xea82;',
+		'icon-facebook2': '&#xea91;',
+		'icon-instagram': '&#xea92;',
+		'icon-twitter': '&#xea96;',
+		'icon-youtube': '&#xea9d;',
+		'icon-search': '&#xe900;',
+		'0': 0
+		},
+		els = document.getElementsByTagName('*'),
+		i, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+}());
